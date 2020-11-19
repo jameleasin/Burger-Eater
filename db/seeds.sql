6767ec5b2bf15ab29d89ab6972a8d8dcc8d973ee
Jamie <Sinclair>
2020-11-19 19:23:41 +0800

    fixed database so burgers can be added

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,5 +2,6 @@
 -- Select the burgers_db
 USE burgers_db;
 -- Insert new rows of data.
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("Beet & Goat Burger", false), ("Mac & Cheeseburger", false), ("I can't Beleive it's Not Meat Burger", false);
+
+INSERT INTO burgers (burger_name) VALUES ('Beet and Goat Burger'), ('Mac and Cheese Burger'), ('Swiss and Mushroom Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avacado Salsa Burger', true), ('Hamburger', false), ('Juicy Lucy Burger', true);
