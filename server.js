f6d9bf098cfb6d2c18e043d6fa739d07b4a02c2e
Jamie Sinclair <62285850+jameleasin@users.noreply.github.com>
2020-11-24 01:41:39 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,21 +4,28 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 
 // ENABLE PORT
-const PORT = process.env.PORT || 56799;
+const PORT = process.env.PORT || 8080;
 
-// CREATE APP
-const app = express();
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// CREATE ROUTES
-const routes = require("./controllers/burgers_controller");
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgersController.js");
+
 app.use(routes);
 
-// LISTEN FOR CONNECTIONS
 app.listen(PORT, function() {
-  console.log(` Server listening on: http://localhost:${PORT}`);
-});
+    console.log("App now listening at localhost:" + PORT);
+  });
