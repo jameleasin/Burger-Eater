365974bbbeabec6d19cc503818696ebbaf302e14
Jamie <Sinclair>
2020-11-23 18:25:20 +0800

    fix connection bug

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 // Connect Node to MySQL.
 var mysql = require("mysql");
 
+//Create connection object
+
+let connection;
+
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
