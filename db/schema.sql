8068c91e05c4a5cb25f88bff498acd70844f2aae
Jamie <Sinclair>
2020-11-23 19:24:39 +0800

    create timestamp

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,11 @@
--- Drops the db if it already exists --
 DROP DATABASE IF EXISTS burgers_db;
--- Create the database burgers_db.
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
--- Create the table burgers.
-CREATE TABLE burgers
-(
-    id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
-    PRIMARY KEY (id)
+CREATE TABLE burgers (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
 );
