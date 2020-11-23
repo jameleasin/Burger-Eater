f9c552af6ec25733b2c1ca278009f075f9af8703
Jamie <Sinclair>
2020-11-23 20:46:03 +0800

    fix server.js bug

--- server.js
+++ server.js
@@ -1,41 +1,25 @@
-// Dependencies
-var express = require("express");
-var exphbs = require("express-handlebars");
-var bodyParser = require("body-parser");
+  
+// DEPENDENCIES
+const express = require("express");
+const exphbs = require("express-handlebars");
+require('dotenv').config()
 
-// Create an instance of the express app.
-var app = express();
+// ENABLE PORT
+const PORT = process.env.PORT || 8080;
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
-
-
-// Serve static content for the app from the "public" directory in the application directory.
+// CREATE APP
+const app = express();
 app.use(express.static("public"));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// parse application/json
-app.use(bodyParser.json());
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-
-// Set Handlebars as the default templating engine.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");;
-
+// CREATE ROUTES
+const routes = require("./controllers/burgers_controller");
 app.use(routes);
 
-
-// Start our server so that it can begin listening to client requests.
+// LISTEN FOR CONNECTIONS
 app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+  console.log(` Server listening on: http://localhost:${PORT}`);
+});
